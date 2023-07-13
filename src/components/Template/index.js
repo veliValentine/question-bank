6279249a2bca29f022d73a40ffcba5389a019c0d
veliValentine <nikkevanukas1995@gmail.com>
2023-07-14 00:15:48 +0300

    feat: template

---
+++ src/components/Template/index.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './Template.css'
+
+const Template = ({
+  saveTemplate,
+  notifyError,
+}) => {
+  const [text, setText] = useState('')
+  const onSubmit = () => {
+    try {
+      const template = JSON.parse(text);
+      saveTemplate(template)
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        notifyError(`Invalid JSON: ${error.message}`)
+      }
+    }
+  }
+  return (
+    <div className='start-container'>
+      <h1>Add Template</h1>
+      <label for="template">Add topics as JSON with or without questions to get started. Skip for empty template</label>
+      <textarea id="template" value={text} onChange={(e) => setText(e.target.value)} />
+      <label for="submit">Submit when ready</label>
+      <button id="submit" onClick={onSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Template;
