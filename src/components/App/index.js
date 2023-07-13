c591e95270d1200d4d2998c464fb24c200c321bf
veliValentine <nikkevanukas1995@gmail.com>
2023-07-14 00:40:16 +0300

    feat: app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,56 @@
-import './App.css';
+import { useState } from 'react';
+import Footer from '../Footer';
+import NavBar from '../NavBar';
+import './App.css'; // https://colorhunt.co/palette/0719520b666a35a29f97feed
+import Template from '../Template';
+import Finish from '../Finish';
+import Questions from '../Questions';
 
-function App() {
+const PAGES = {
+  TEMPLATE: 'template',
+  ADD: 'add',
+  FINISH: 'finish'
+}
+
+const App = () => {
+  const [page, setPage] = useState(PAGES.TEMPLATE);
+  const [template, setTemplate] = useState({})
+
+  const notifyError = (message) => console.error(message)
+
+  const saveTemplate = (template) => {
+    setTemplate(template)
+  }
+
+  const choosePage = (page) => {
+    switch (page) {
+      case PAGES.ADD:
+        return <Questions />
+      case PAGES.FINISH:
+        return <Finish result={template} />
+      case PAGES.TEMPLATE:
+      default:
+        return <Template
+          template={template}
+          notifyError={notifyError}
+          saveTemplate={saveTemplate}
+        />
+    }
+  }
+
+  const pageNavigator = {
+    template: () => setPage(PAGES.TEMPLATE),
+    add: () => setPage(PAGES.ADD),
+    finish: () => setPage(PAGES.FINISH),
+  }
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='app-container'>
+      <NavBar pageNavigator={pageNavigator} />
+      <div className='content-container'>
+        {choosePage(page)}
+      </div>
+      <Footer />
     </div>
   );
 }
